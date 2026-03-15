208aad89492c048eff1dcacd4cb533b9bc9ff7fc
icamilohernandez <icamilohernandez@unicesar.edu.co>
2026-03-15 18:41:48 -0500

    agregar registrarVehiculo y ListaVehiculos

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -2,10 +2,14 @@ package view;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+
 import model.Vehiculo;
 import model.Conductor;
 import model.Pasajero;
 import model.Ticket;
+import service.VehiculoService;
+import service.PersonaService;
+import service.TicketService;
 
 public class Menu {
     
@@ -15,6 +19,10 @@ public class Menu {
     private ArrayList<Ticket>tickets=new ArrayList<>();
     private Scanner sc = new Scanner(System.in);
 
+    private VehiculoService vehiculoService = new VehiculoService();
+    private PersonaService  personaService  = new PersonaService();
+    private TicketService   ticketService   = new TicketService();
+
     public void mostrar() {
         int op;
         do {
@@ -22,27 +30,53 @@ public class Menu {
             System.out.println("1. Registrar Vehiculo");
             System.out.println("2. Listar vehiculos");
             System.out.println("3. Registrar Conductor");
-            System.out.println("4. Registrar conductor");
-            System.out.println("5. Registrar pasajero");
-            System.out.println("6. Vender ticket");
-            System.out.println("7. Listar tickets");
-            System.out.println("8. Estadisticas");
+            System.out.println("4. Registrar pasajero");
+            System.out.println("5. Vender ticket");
+            System.out.println("6. Listar tickets");
+            System.out.println("7. Estadisticas");
             System.out.println("0. Salir");
             System.out.print("Opcion: ");
             op = sc.nextInt();
 
             switch (op){
-                case 1: break; // registrarVehiculo
-                case 2: break; // listarVehiculos
-                case 3: break; // registrarConductor
-                case 4: break; // registrarPasajero
-                case 5: break; // venderTicket
-                case 6: break; // listarTickets
-                case 7: break; // verEstadisticas
+                case 1: registrarVehiculo(); break;
+                case 2: listarVehiculos(); break;
+                case 3: registrarConductor(); break;
+                case 4: registrarPasajero(); break;
+                case 5: venderTicket(); break;
+                case 6: listarTickets(); break;
+                case 7: verEstadisticas(); break;
                 case 0: System.out.println("Adios"); break;
                 default: System.out.println("Opcion invalida");
             }
 
         } while (op != 0);
     }
+
+    private void registrarVehiculo() {
+        sc.nextLine();
+        System.out.println("Registrar Vehiculo");
+        System.out.print("Placa: ");
+        String placa = sc.nextLine();
+        System.out.print("ruta: ");
+        String ruta = sc.nextLine();
+        System.out.print("Modelo: ");
+        String modelo = sc.nextLine();
+        System.out.println("Tipo: 1.Buseta  2.MicroBus  3.Bus");
+        int tipo = sc.nextInt();
+        System.out.println(vehiculoService.registrar(vehiculos, placa, ruta, modelo));
+
+    }
+    private void listarVehiculos() {
+        if (vehiculos.isEmpty()) {
+            System.out.println("No hay vehiculos registrados");
+            return;
+        }
+        System.out.println("Lista de Vehiculos:");
+        for (Vehiculo v : vehiculos) {
+            System.out.println(v);
+        }
+
+    }
+
 }
