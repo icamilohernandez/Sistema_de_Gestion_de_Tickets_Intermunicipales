3cfd94ae28e1bf08adee8a23feed7d2a37c28921
icamilohernandez <icamilohernandez@unicesar.edu.co>
2026-03-15 17:14:57 -0500

    feat: crear estructura base de Main con ArrayLists

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+import view.Menu;
 package view;
 
-/**
- *
- * @author ivanc
- */
 public class Main {
-    
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.showMenu();
+    }
 }
--- src/view/Menu.java
+++ src/view/Menu.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package view;
 
-/**
- *
- * @author ivanc
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+import model.Vehiculo;
+import model.Conductor;
+import model.Pasajero;
+import model.Ticket;
+
 public class Menu {
     
+    private ArrayList<Vehiculo>vehiculos=new ArrayList<>();
+    private ArrayList<Conductor>conductores=new ArrayList<>();
+    private ArrayList<Pasajero>pasajeros=new ArrayList<>();
+    private ArrayList<Ticket>tickets=new ArrayList<>();
+    private Scanner sc = new Scanner(System.in);
+
+    public void mostrar() {
+        int op;
+        do {
+            System.out.println("TransCesar S.A.S");
+            System.out.println("1. Registrar Vehiculo");
+            System.out.println("2. Listar vehiculos");
+            System.out.println("3. Registrar Conductor");
+            System.out.println("4. Registrar conductor");
+            System.out.println("5. Registrar pasajero");
+            System.out.println("6. Vender ticket");
+            System.out.println("7. Listar tickets");
+            System.out.println("8. Estadisticas");
+            System.out.println("0. Salir");
+            System.out.print("Opcion: ");
+            op = sc.nextInt();
+
+            switch (op){
+                case 1: break; // registrarVehiculo
+                case 2: break; // listarVehiculos
+                case 3: break; // registrarConductor
+                case 4: break; // registrarPasajero
+                case 5: break; // venderTicket
+                case 6: break; // listarTickets
+                case 7: break; // verEstadisticas
+                case 0: System.out.println("Adios"); break;
+                default: System.out.println("Opcion invalida");
+            }
+
+        } while (op != 0);
+    }
 }
