37f57861ff7ae262ff26a003196584d6ef706964
Samuel David Caro Borrero <sdavidcaro@unicesar.edu.co>
2026-03-21 14:46:23 -0500

    feat: agregar encapsulamiento y toString a clase Reserva

--- src/model/Reserva.java
+++ src/model/Reserva.java
@@ -4,10 +4,82 @@
  */
 package model;
 
-/**
- *
- * @author Csamu
- */
+import java.time.LocalDate;
+
 public class Reserva {
-    
+
+    private String codigo;
+    private Pasajero pasajero;
+    private Vehiculo vehiculo;
+    private LocalDate fechaCreacion;
+    private LocalDate fechaViaje;
+    private String estado;
+
+    public Reserva() {}
+    public Reserva(String codigo, Pasajero pasajero, Vehiculo vehiculo, LocalDate fechaCreacion, LocalDate fechaViaje, String estado) {
+        this.codigo = codigo;
+        this.pasajero = pasajero;
+        this.vehiculo = vehiculo;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaViaje = fechaViaje;
+        this.estado = estado;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Pasajero getPasajero() {
+        return pasajero;
+    }
+
+    public void setPasajero(Pasajero pasajero) {
+        this.pasajero = pasajero;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public LocalDate getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDate fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public LocalDate getFechaViaje() {
+        return fechaViaje;
+    }
+
+    public void setFechaViaje(LocalDate fechaViaje) {
+        this.fechaViaje = fechaViaje;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Codigo: " + codigo +
+               " | Pasajero: " + pasajero.getNombre() +
+               " | Vehiculo: " + vehiculo.getPlaca() +
+               " | Fecha Creacion: " + fechaCreacion +
+               " | Fecha Viaje: " + fechaViaje +
+               " | Estado: " + estado;
+    }
 }
