b0b8d307b4875a51ee37bbdf8b14ce97ffafba76
carlos daniel ospino <carlosdospino@unicesar.edu.co>
2026-03-21 16:23:53 -0500

    feat: implementar validacion de capacidad para nueva reserva

--- src/service/ReservaService.java
+++ src/service/ReservaService.java
@@ -1,9 +1,25 @@
 package service;
 
+import dao.ReservaDAO;
+import dao.ReservaDAOImpl;
+import model.Vehiculo;
+
 public class ReservaService {
+
+    private ReservaDAO reservaDAO;
+    private VehiculoService vehiculoService;
+    private PersonaService personaService;
 
     private static int contador = 1;
 
-    public ReservaService() {
+    public ReservaService(PersonaService personaService, VehiculoService vehiculoService) {
+        this.reservaDAO     = new ReservaDAOImpl();
+        this.personaService = personaService;
+        this.vehiculoService = vehiculoService;
+    }
+
+
+    private boolean validarCapacidad(String placaVehiculo) {
+        return vehiculoService.verificarDisponibilidad(placaVehiculo);
     }
 }
